4500fad26d0932eee79c1af0b31692d2ec062556
sunzhiyong <dominick.szy@alibaba-inc.com>
2019-10-31 10:31:53 +0700

    feat: export

--- src/read.js
+++ src/read.js
@@ -1,5 +1,14 @@
 const recast = require("recast");
+const TNT = recast.types.namedTypes
 
-recast.run( function(ast, printSource){
-  printSource(ast)
-})
+
+recast.run(function(ast, printSource) {
+  recast.visit(ast, {
+    visitExpressionStatement: function(path) {
+      const node = path.node
+      // 判断是否为ExpressionStatement，正确不输出，错误则全局报错
+      console.log(TNT.ExpressionStatement.assert(node))
+      this.traverse(path);
+    }
+  });
+});
